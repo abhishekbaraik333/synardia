dfaeea091dac4ca31c6f547dcb224cb5ec715276
abhishekbaraik333 <abhitamangme@gmail.com>
2024-09-14 17:59:39 +0530

    on krke

--- script.js
+++ script.js
@@ -14,39 +14,39 @@ window.addEventListener('load', function() {
 });
 
 
-// document.addEventListener('DOMContentLoaded', function() {
-//     new fullpage('#fullpage', {
-//       anchors: ['section1', 'section2', 'section3', 'section4','section5','section6','section7','section8'], // Section anchors for URL
-//       scrollingSpeed: 1000, // Smooth scrolling speed
-//       scrollOverflow: true, // Enable scroll for overflowing content in sections
-//     });
+document.addEventListener('DOMContentLoaded', function() {
+    new fullpage('#fullpage', {
+      anchors: ['section1', 'section2', 'section3', 'section4','section5','section6','section7','section8'], // Section anchors for URL
+      scrollingSpeed: 1000, // Smooth scrolling speed
+      scrollOverflow: true, // Enable scroll for overflowing content in sections
+    });
 
-//     document.getElementById('arrow-down1').addEventListener('click', function() {
-//       fullpage_api.moveSectionDown();
-//     });
-//     document.getElementById('arrow-down2').addEventListener('click', function() {
-//       fullpage_api.moveSectionDown();
-//     });
-//     document.getElementById('arrow-down3').addEventListener('click', function() {
-//       fullpage_api.moveSectionDown();
-//     });
-//     document.getElementById('arrow-down4').addEventListener('click', function() {
-//       fullpage_api.moveSectionDown();
-//     });
-//     document.getElementById('arrow-down5').addEventListener('click', function() {
-//       fullpage_api.moveSectionDown();
-//     });
-//     document.getElementById('arrow-down6').addEventListener('click', function() {
-//       fullpage_api.moveSectionDown();
-//     });
-//     document.getElementById('arrow-down7').addEventListener('click', function() {
-//       fullpage_api.moveSectionDown();
-//     });
-//     document.getElementById('arrow-down8').addEventListener('click', function() {
-//       fullpage_api.moveTo(1);
-//     });
+    document.getElementById('arrow-down1').addEventListener('click', function() {
+      fullpage_api.moveSectionDown();
+    });
+    document.getElementById('arrow-down2').addEventListener('click', function() {
+      fullpage_api.moveSectionDown();
+    });
+    document.getElementById('arrow-down3').addEventListener('click', function() {
+      fullpage_api.moveSectionDown();
+    });
+    document.getElementById('arrow-down4').addEventListener('click', function() {
+      fullpage_api.moveSectionDown();
+    });
+    document.getElementById('arrow-down5').addEventListener('click', function() {
+      fullpage_api.moveSectionDown();
+    });
+    document.getElementById('arrow-down6').addEventListener('click', function() {
+      fullpage_api.moveSectionDown();
+    });
+    document.getElementById('arrow-down7').addEventListener('click', function() {
+      fullpage_api.moveSectionDown();
+    });
+    document.getElementById('arrow-down8').addEventListener('click', function() {
+      fullpage_api.moveTo(1);
+    });
 
-//   });
+  });
 
 
 gsap.to(".circle-2", {
